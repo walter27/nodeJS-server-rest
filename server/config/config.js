2dd0db8a5a1fb8b2f5b9ded09eeba1f9ce8fe5a0
walter <walter952012@gmail.com>
2020-05-13 19:14:51 -0500

    Configuracion de la URL para la conexion a la base  de datos mongoose atlas IV

--- server/config/config.js
+++ server/config/config.js
@@ -8,11 +8,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 //base  de datos
-//if (process.env.NODE_ENV === 'dev') {
- //   urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
     urlDB = 'mongodb+srv://mongo:1234@cluster0-iyyjm.mongodb.net/test?retryWrites=true&w=majority';
-//}
+}
 
 process.env.URLDB = urlDB;
 
--- server/config/dist/config.dev.js
+++ server/config/dist/config.dev.js
@@ -5,10 +5,11 @@ process.env.PORT = process.env.PORT || 3000; //entorno
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 var urlDB; //base  de datos
-//if (process.env.NODE_ENV === 'dev') {
-//   urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
 
-urlDB = 'mongodb+srv://mongo:1234@cluster0-iyyjm.mongodb.net/test?retryWrites=true&w=majority'; //}
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB = 'mongodb+srv://mongo:1234@cluster0-iyyjm.mongodb.net/test?retryWrites=true&w=majority';
+}
 
 process.env.URLDB = urlDB;
