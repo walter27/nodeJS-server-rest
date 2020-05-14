8b3f109b28923b13d54504d1162f5d699de8826b
walter <walter952012@gmail.com>
2020-05-13 19:35:09 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://mongo:1234@cluster0-iyyjm.mongodb.net/test?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
--- server/config/dist/config.dev.js
+++ server/config/dist/config.dev.js
@@ -9,7 +9,7 @@ var urlDB; //base  de datos
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = 'mongodb+srv://mongo:1234@cluster0-iyyjm.mongodb.net/test?retryWrites=true&w=majority';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
